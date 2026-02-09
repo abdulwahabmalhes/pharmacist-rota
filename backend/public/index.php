1c070284367f058740058587df75874ba141a4a2
Abdulwahab Malhes <abdulwahab.malhes@gmail.com>
2026-02-09 23:56:32 +0400

    Add direct CORS headers

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+// Handle CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
