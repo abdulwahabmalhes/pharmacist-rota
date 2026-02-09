36b32dab57261d66b3225def087aeb98b637798a
Abdulwahab Malhes <abdulwahab.malhes@gmail.com>
2026-02-09 23:52:00 +0400

    Allow all CORS origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,9 +6,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://frontend-gold-pi-55.vercel.app', 'https://*.vercel.app'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
 
     'allowed_headers' => ['*'],
 
